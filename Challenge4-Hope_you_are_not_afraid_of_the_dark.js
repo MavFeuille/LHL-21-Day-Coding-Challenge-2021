42117a589179c5bb7287db5a82e6b4abcdb84c6e
MavFeuille <mavis.feuille@gmail.com>
2021-11-25 23:56:07 +0800

    Remove console.log

--- Challenge4-Hope_you_are_not_afraid_of_the_dark.js
+++ Challenge4-Hope_you_are_not_afraid_of_the_dark.js
@@ -6,18 +6,10 @@ const toggle = {
 const switchToggle = (toggle) => {
 
  if (!toggle.isOn) {
-   console.log("BEFORE switch toggle.isOn status: " , toggle.isOn);
    toggle.isOn = true;
-   console.log("AFTER switch toggle.isOn status: ", toggle.isOn);
-   console.log("Toggle: ", toggle);
    return toggle;
  } else {
-  console.log("BEFORE switch toggle.isOn status: " , toggle.isOn);
   toggle.isOn = false;
-  console.log("AFTER switch toggle.isOn status: ", toggle.isOn);
-  console.log("Toggle: ", toggle);
   return toggle;
  }
 }
-
-console.log(switchToggle(toggle));
