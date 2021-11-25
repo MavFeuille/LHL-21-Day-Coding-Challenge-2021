8bafd7651fc1758791d5627ca9138a24a2a219a3
MavFeuille <mavis.feuille@gmail.com>
2021-11-25 23:33:07 +0800

    Remove redundant comments

--- Challenge2-one_small_step_at_a_time.js
+++ Challenge2-one_small_step_at_a_time.js
@@ -1,5 +1,3 @@
 const generateAstronautTag = (astronaut) => {
-  // Code here!
  return `${astronaut.prefix}: ${astronaut.firstName} "${astronaut.nickname}" ${astronaut.lastName}`
-  // Remember to return a value!
 }
