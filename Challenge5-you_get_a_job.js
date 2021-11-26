ccc6b0784553b0ce18bf03b17f8bfac2b8376964
MavFeuille <mavis.feuille@gmail.com>
2021-11-26 15:53:52 +0800

    Create function to add job to astronaut object

--- Challenge5-you_get_a_job.js
+++ Challenge5-you_get_a_job.js
@@ -1,3 +1,16 @@
-const addJobToAstronaut = (astronaut, job) => {
-  
+const astronaut = {
+  firstName:"Chris",
+  lastName: "Hadfield",
+  nickname:"Space Oddity",
+  prefix:"Astronaut"
 }
+
+const job = "Shuttle DJ";
+
+const addJobToAstronaut = (astronaut, job) => {
+  astronaut = Object.assign(astronaut, {job: `${job}`})
+  console.log("astronaut: ", astronaut)
+  return astronaut
+}
+
+addJobToAstronaut(astronaut, job)
