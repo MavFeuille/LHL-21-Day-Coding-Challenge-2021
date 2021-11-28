b4f9ea6affd0aca255209841ed2dde1606fcc44d
MavFeuille <mavis.feuille@gmail.com>
2021-11-28 22:32:21 +0800

    Set up variables

--- Challenge7-its_a_plane.js
+++ Challenge7-its_a_plane.js
@@ -1,3 +1,8 @@
+const temperature = 32
+const condition = "Sunny with small clouds"
+const windSpeed = 20
+const windDirection = "NNE
+
 const storeWeatherConditions = (temperature, condition, windSpeed, windDirection) => {
   // Code here!
 
