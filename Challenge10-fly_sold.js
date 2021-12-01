ff4abb015e653edf019232c80c46ee88b66fcfb3
MavFeuille <mavis.feuille@gmail.com>
2021-12-02 00:44:10 +0800

    Create function to return average wind speed

---
+++ Challenge10-fly_sold.js
@@ -0,0 +1,29 @@
+const weatherEntries = [
+  { 
+    temperature:0, 
+    weather:"sunny", 
+    windDirection: "NNE", 
+    windSpeed:44.74 
+  },
+  { 
+    temperature:10, 
+    weather:"cloudy", 
+    windDirection: "NNE", 
+    windSpeed:14.74 
+  }
+]  
+
+const averageWindSpeed = (weatherEntries) => {
+  let sum = 0;
+  let average = 0;
+
+  for (entry of weatherEntries) {
+    sum += entry['windSpeed'];
+  }
+  // console.log("sum: ", sum);
+  average = Math.round(sum / weatherEntries.length);
+  console.log("average: ", average);
+  return average;
+}
+
+averageWindSpped(weatherEntries)
