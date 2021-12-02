d85459db59ffcb40b2a297e0c1751b76708cc4c6
MavFeuille <mavis.feuille@gmail.com>
2021-12-03 00:24:27 +0800

    Create variables

---
+++ Challenge11-save_the_date.js
@@ -0,0 +1,16 @@
+const missionDate = "2021-12-12"
+const platformList = [
+  {
+    name:"Platform A",
+    bookDate:"2021-12-11"
+  },
+  {
+    name:"Platform B",
+    bookDate:undefined
+  },
+  {
+    name:"Platform C",
+    bookDate:undefined
+  },
+] 
+
