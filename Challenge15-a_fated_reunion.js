0fce72483be87b37b6b3f6f910fbd1001ed678c7
MavFeuille <mavis.feuille@gmail.com>
2021-12-09 01:16:29 +0800

    Create timeRemaining function

--- Challenge15-a_fated_reunion.js
+++ Challenge15-a_fated_reunion.js
@@ -4,15 +4,20 @@ const missionName = "Moon visit";
 
 const timeRemaining = (launchDate, missionName, fakeToday) => {
   const today = fakeToday || new Date() // Do not alter this line!
-  let output = {};
+  
 
   const newLaunchDate = new Date(launchDate);
   const newToday = new Date (today);
   const timeRemaining = Math.abs(newLaunchDate - newToday);
   const daysRemaining = Math.ceil(timeRemaining / (1000 * 60 * 60 * 24));
 
-  
-  
+  let output = {
+    missionName: `${missionName}`,
+    daysRemaining: `${daysRemaining}`
+  };
+
+  // console.log("output: ", output)
+  return output;
 }
 
 timeRemaining(launchDate, missionName, fakeToday)
