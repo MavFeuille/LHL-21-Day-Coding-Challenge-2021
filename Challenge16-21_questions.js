791dfba25fd1353c7a3e4cc4251d0571325f8521
MavFeuille <mavis.feuille@gmail.com>
2021-12-09 01:27:10 +0800

    Remove console log

--- Challenge16-21_questions.js
+++ Challenge16-21_questions.js
@@ -2,7 +2,17 @@ const firstPosition = {
   time: 1637074462,
   altitude: 1100
 }
+
 const secondPosition = {
   time: 1637074558,
   altitude: 2200
 }
+
+const getAverageSpeed = (firstPosition, secondPosition) => {
+  const altitudeDifference = secondPosition.altitude - firstPosition.altitude
+  const timeDiffereence = secondPosition.time - firstPosition.time
+  const averageSpeed = (altitudeDifference / timeDiffereence).toFixed(1);
+  return parseFloat(averageSpeed);
+}
+
+getAverageSpeed(firstPosition, secondPosition);
