561ac0f4f01f0c7b03e8a9c0321948721e603322
MavFeuille <mavis.feuille@gmail.com>
2021-12-09 01:05:27 +0800

    Create new variables

--- Challenge15-a_fated_reunion.js
+++ Challenge15-a_fated_reunion.js
@@ -1,3 +1,18 @@
-const launchDate = "2021-12-12"
-const fakeToday = "2021-12-01"
-const missionName = "Moon visit"
+const launchDate = "2021-12-12";
+const fakeToday = "2021-12-01";
+const missionName = "Moon visit";
+
+const timeRemaining = (launchDate, missionName, fakeToday) => {
+  const today = fakeToday || new Date() // Do not alter this line!
+  let output = {};
+
+  const newLaunchDate = new Date(launchDate);
+  const newToday = new Date (today);
+  const timeRemaining = Math.abs(newLaunchDate - newToday);
+  const daysRemaining = Math.ceil(timeRemaining / (1000 * 60 * 60 * 24));
+
+  
+  
+}
+
+timeRemaining(launchDate, missionName, fakeToday)
