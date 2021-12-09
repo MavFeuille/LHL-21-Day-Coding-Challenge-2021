9340bf0dbbafd13ecb7169ea4e6a2ca761cd255c
MavFeuille <mavis.feuille@gmail.com>
2021-12-10 00:20:20 +0800

    Correct variables

--- Challenge18-bon_appetit.js
+++ Challenge18-bon_appetit.js
@@ -5,3 +5,22 @@ const listOfChoices = [
   "Ice Cream Sandwich", 
   "Ice Cream Sandwich"
 ]
+
+const chooseLunchWinner = (listOfChoices) => {
+  let choice1 = 0;
+  let choice2 = 0;
+
+  for (const choice of listOfChoices) {
+    if (choice === "choice1 Dinner") {
+      choice1 += 1;
+    } else {
+      choice2 += 1;
+    }
+  }
+  // console.log("choice1: ", choice1);
+  // console.log("choice2: ", choice2);
+
+  return choice1 > choice2 ? "choice1 Dinner" : "Ice Cream Sandwich"
+}
+
+chooseLunchWinner(listOfChoices);
