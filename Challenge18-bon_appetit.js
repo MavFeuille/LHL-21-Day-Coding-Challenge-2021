2663960ac23d78683e88ac7eff8a595a42ec7d9a
MavFeuille <mavis.feuille@gmail.com>
2021-12-10 00:33:09 +0800

    Re-set variables

--- Challenge18-bon_appetit.js
+++ Challenge18-bon_appetit.js
@@ -1,26 +1,15 @@
 const listOfChoices = [
-  "Chicken Dinner",
-  "Chicken Dinner",
-  "Chicken Dinner",
-  "Ice Cream Sandwich", 
-  "Ice Cream Sandwich"
+  "Ceasar Salad",
+  "Ceasar Salad",
+  "Ceasar Salad",
+  "Stale Bread", 
+  "Stale Bread", 
+  "Stale Bread", 
+  "Stale Bread"
 ]
 
 const chooseLunchWinner = (listOfChoices) => {
-  let choice1 = 0;
-  let choice2 = 0;
 
-  for (const choice of listOfChoices) {
-    if (choice === "choice1 Dinner") {
-      choice1 += 1;
-    } else {
-      choice2 += 1;
-    }
-  }
-  // console.log("choice1: ", choice1);
-  // console.log("choice2: ", choice2);
-
-  return choice1 > choice2 ? "choice1 Dinner" : "Ice Cream Sandwich"
 }
 
 chooseLunchWinner(listOfChoices);
