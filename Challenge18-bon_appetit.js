df0cec01038c78d6e3faa58940f8d886b7c311f1
MavFeuille <mavis.feuille@gmail.com>
2021-12-10 00:42:57 +0800

    Create function to choose lunch winner

--- Challenge18-bon_appetit.js
+++ Challenge18-bon_appetit.js
@@ -9,7 +9,9 @@ const listOfChoices = [
 ]
 
 const chooseLunchWinner = (listOfChoices) => {
-
+  let choice1 = listOfChoices.filter(choice => choice === listOfChoices[0])
+  let choice2 = listOfChoices.filter(choice => choice === listOfChoices[listOfChoices.length-1])
+  
+  return choice1.length > choice2.length ? choice1[0] : choice2[0]
 }
-
 chooseLunchWinner(listOfChoices);
