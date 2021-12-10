f96bc700e98b5418c39de2f9bccb5ddec187b93e
MavFeuille <mavis.feuille@gmail.com>
2021-12-11 02:26:23 +0800

    Set up variables

---
+++ Challenge19-its_pumpkin_spice_time.js
@@ -0,0 +1,8 @@
+const receivedData = [
+  {type: "astro", data: "Saturn Data"},
+  {type: "bio", data: "Space Potatoes"},
+  {type: "physics", data: "Lagrange Points"},
+  {type: "bio", data: "OMG Tardigrades"},
+  {type: "physics", data: "Material reflectivity"},
+  {type: "astro", data: "Mercury is not the hottest"},
+]
