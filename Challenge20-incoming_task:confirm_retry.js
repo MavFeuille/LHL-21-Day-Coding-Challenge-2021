57e6454a91daff04a3183690ba09fc0155db2779
MavFeuille <mavis.feuille@gmail.com>
2021-12-12 01:56:43 +0800

    Set up variables

---
+++ Challenge20-incoming_task:confirm_retry.js
@@ -0,0 +1,18 @@
+const speed = 40
+  const missionData = {
+    astro:["...","..."], 
+    bio:["..."], 
+    physics:["..."]
+  }
+
+  const checks = {
+    maxSpeed:50,
+    minSpeed:20,
+    dataEntries:{
+      astro:3,
+      bio:1,
+      physics:1
+    }
+  }
+
+  
