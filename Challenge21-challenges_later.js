ff28d62091d2621933c371578fdf0d4918df1f8d
MavFeuille <mavis.feuille@gmail.com>
2021-12-13 00:13:30 +0800

    Set up variables

---
+++ Challenge21-challenges_later.js
@@ -0,0 +1,11 @@
+const exchanges = [
+  {origin:"MC", message:"So how is it out there?"},
+  {origin:"Shuttle", message:"Oh it's pretty nice!"},
+  {origin:"MC", message:"Did you like the challenges?"},
+]
+   
+const missionData = {
+  astro:["...","..."], 
+  bio:["..."], 
+  physics:["..."]
+}
